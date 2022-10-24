7d2af8ec5ede8dd8985c71b35dca47632bd7727e
Bárdosi_Bálint <bbalintb@stud.uni-obuda.hu>
2022-10-24 21:34:38 +0200

    Add project files.

---
+++ AYZ8R9_SOF_202231/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AYZ8R9_SOF_202231.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
