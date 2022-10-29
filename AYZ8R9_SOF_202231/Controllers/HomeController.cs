a18197dacd85696c929aa0416c9b41842d7b9e6e
SalaminBence <bene.salamin@gmail.com>
2022-10-29 19:35:10 +0200

    Written PromoteLogic (Dependency Injection Error)

--- AYZ8R9_SOF_202231/Controllers/HomeController.cs
+++ AYZ8R9_SOF_202231/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
-﻿using AYZ8R9_SOF_202231.Models;
+﻿using AYZ8R9_SOF_202231.Data;
+using AYZ8R9_SOF_202231.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 using System.Diagnostics;
 
 namespace AYZ8R9_SOF_202231.Controllers
@@ -11,12 +13,14 @@ namespace AYZ8R9_SOF_202231.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly UserManager<IdentityUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SCRUMDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        public HomeController(ILogger<HomeController> logger, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, SCRUMDbContext db)
         {
             _logger = logger;
             _userManager = userManager;
             _roleManager = roleManager;
+            _db = db;
         }
 
         public IActionResult Index()
@@ -33,23 +37,29 @@ namespace AYZ8R9_SOF_202231.Controllers
         {
             var actual = this.User;
             var user = await _userManager.GetUserAsync(actual);
-            var role = new IdentityRole()
-            {
-                Name = "Scrum_Master"
-            };
-            if (! await _roleManager.RoleExistsAsync("Scrum_Master"))
-            {
-                await _roleManager.CreateAsync(role);
-            }
             await _userManager.AddToRoleAsync(user, "Scrum_Master");
             return RedirectToAction(nameof(Index));
         }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult DemoteMaster(string userId) 
+        {
+            var item = _db.AppUsers.FirstOrDefault(u => u.Id == userId);
+            if (item != null)
+            {
+                 _userManager.RemoveFromRoleAsync(item, "Scrum_Master");
+            }
+
+            return View(_db.Users);
+
+        }
+
         [Authorize(Roles = "Admin,Scrum_Master")]
         public IActionResult Promote() 
         {
 
 
-            return Promote();
+            return View(_db.Users);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
