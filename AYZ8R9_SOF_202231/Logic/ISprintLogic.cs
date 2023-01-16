aff1bd60c78c0faba35c07cc568c23613e2ce342
Bárdosi_Bálint <bbalintb@stud.uni-obuda.hu>
2023-01-16 19:51:56 +0100

    Sprintlogic and ISprintLogic created

---
+++ AYZ8R9_SOF_202231/Logic/ISprintLogic.cs
@@ -0,0 +1,13 @@
+﻿using AYZ8R9_SOF_202231.Model;
+
+namespace AYZ8R9_SOF_202231.Logic
+{
+    public interface ISprintLogic
+    {
+        void ChangeSprint(Sprint newSprint);
+        void CreateSprint(Sprint newSprint);
+        void DeleteSprint(string id);
+        IEnumerable<Sprint> GetAllSprints();
+        Sprint GetOneSprint(string id);
+    }
+}
