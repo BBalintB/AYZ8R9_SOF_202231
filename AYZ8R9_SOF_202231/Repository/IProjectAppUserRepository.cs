8b6f19b8ef547112d7b46bc2f4fcb1f0391551b7
Bárdosi_Bálint <bbalintb@stud.uni-obuda.hu>
2023-01-17 19:38:46 +0100

    Repository extended

---
+++ AYZ8R9_SOF_202231/Repository/IProjectAppUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using AYZ8R9_SOF_202231.Model;
+
+namespace AYZ8R9_SOF_202231.Repository
+{
+    public interface IProjectAppUserRepository
+    {
+        void Change(ProjectAppUser newPA);
+        void Create(ProjectAppUser newPA);
+        void Delete(string id);
+        ProjectAppUser GetOne(string id);
+        IQueryable<ProjectAppUser> GetAll();
+    }
+}
