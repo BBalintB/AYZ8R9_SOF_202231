448761be6e9bc81ee5b8fcf12bfddc0e9ed19678
Bárdosi_Bálint <bbalintb@stud.uni-obuda.hu>
2023-01-30 12:26:09 +0100

    WPF designed and SignalR created

---
+++ AYZ8R9_SOF_202231/Hubs/EventHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace AYZ8R9_SOF_202231.Hubs
+{
+    public class EventHub:Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            Clients.Caller.SendAsync("Connected", Context.ConnectionId);
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
